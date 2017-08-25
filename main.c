570c85c81cc4e631589f751ae73c429f3d755f8f
Folbort <ioness-heorgx.folbort@intel.com>
2017-08-25 14:29:13 +0300

    added module parameter initialisation

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
+/*
+*alov parameter to be forvarded into module
+*Next permitions are allowed:
+	R - read
+	W - write
+	X - execute
+*Next user instances allowed:
+*	USR - user (only superuser)
+*	GRP - grop (all users in sudo group)
+* 	OTH - all users  (all users)
+*	UGO - user, gorup and others
+*use next makroses in thisrd parameter:
+*	S_I<P><USR>
+*	Where 
+*		<P> - permission letter, specified abow
+*		<USR> - user instance, specified abow
+*/
+int parameter_1 = 0;
+int parameter_2 = 0;
+int parameter_3 = 0;
+int parameter_4 = 0;
+module_param(parameter_1, int, S_IRUSR|S_IWUSR);
+module_param(parameter_2, int, S_IRUSR);
+module_param(parameter_3, int, S_IWUSR);
+module_param(parameter_4, int, 0);
+
+#define MY_ARRAY_SIZE 4
+int* array_param[MY_ARRAY_SIZE] = {&parameter_1, &parameter_2, &parameter_3, &parameter_4};
+
 
 
 /*
@@ -8,7 +37,14 @@
  * this function after it will be executed
  */
 static int __init hello_init(void){
-    printk("this %s will %s some %s\n", "message", "contain", "strings");
+
+	unsigned char i;
+	for(i = 0; i < MY_ARRAY_SIZE; i++)
+	{
+		printk("parameter_%i = %d", i, *array_param[i]);
+	}
+	printk("initilaisation done");
+
 	return 0;
 }
 module_init(hello_init);
